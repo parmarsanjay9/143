22eed733e6a06cbfa045f94ff17f04a8b7397142
parmarsanjay9 <100059162+parmarsanjay9@users.noreply.github.com>
2022-12-09 21:12:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+song1 = " ";
+song2 = " ";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+songStatus = " ";
+scoreLeftWrist = 0;
+
+function preload(){
+    song1 = loadSound('music.mp3');
+    song2 = loadSound('music2.mp3');
+}
+
+function setup(){
+    canvas = createCanvas(500, 400);
+    canvas.position(400,200);
+
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log("PoseNet Is Intialized");
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0){
+        console.log(results);
+
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+    }
+}
+
+function draw(){
+    image(video, 0, 0, 500, 400);
+
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    songStatus = song1.isPlaying();
+    if(scoreLeftWrist > 0.2){
+        circle(leftWristX,leftWristY,20);
+        song2.stop();
+        if(songStatus == false){
+            song1.play();
+            document.getElementById("song_name").innerHTML = "peter pam song";
+        }
+    }
+}
